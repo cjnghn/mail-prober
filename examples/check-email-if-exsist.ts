4606bb927ab14431a37744be24354ebd9cb49c04
dan <50140505+steely-d@users.noreply.github.com>
2023-03-04 18:04:02 +0900

    add example

---
+++ examples/check-email-if-exsist.ts
@@ -0,0 +1,23 @@
+import { checkEmail } from "../src/index";
+
+async function wait(ms: number) {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(null), ms);
+  });
+}
+
+async function main() {
+  const reachable: string[] = [];
+  for (let i = 100; i < 1000; i++) {
+    const resp = await checkEmail(`chotnt${i}@naver.com`);
+    if (resp.reachable) reachable.push(resp.email);
+
+    console.log(JSON.stringify(resp, null, 2));
+
+    await wait(500);
+  }
+
+  console.log("📮 REACHABLE > ", reachable);
+}
+
+main();
